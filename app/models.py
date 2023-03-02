27b002c44f07e36a09deab17a7bc83b93d926cbc
ruslan-kornich <korny4rus143@gmail.com>
2023-03-02 10:47:01 +0200

    models

--- app/models.py
+++ app/models.py
@@ -23,12 +23,16 @@ class SchemaBasicInfo(BaseModel):
         ("'", "Single-quote(')"),
         ("'''", "Triple-quote(''')"),
         ('"""', 'Triple-quote(""")'),
-        ('()', 'Bracket(())'),
-        ('[]', 'Straight Brackets([])'),
+        ("()", "Bracket(())"),
+        ("[]", "Straight Brackets([])"),
     )
     schema_name = models.CharField(max_length=20, null=True)
-    column_separator = models.CharField(max_length=15, choices=SEPARATOR_CHOICES, default=";")
-    string_character = models.CharField(max_length=15, choices=STRING_CHARACTER_CHOICES, default="'")
+    column_separator = models.CharField(
+        max_length=15, choices=SEPARATOR_CHOICES, default=";"
+    )
+    string_character = models.CharField(
+        max_length=15, choices=STRING_CHARACTER_CHOICES, default="'"
+    )
     creator = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
@@ -47,8 +51,10 @@ class SchemaColumns(BaseModel):
         ("int", "Integer"),
         ("date", "Date"),
     )
-    column_name = models.CharField(max_length=20, default='', null=True)
-    column_type = models.CharField(max_length=10, choices=TYPE_CHOICES, default="full_name")
+    column_name = models.CharField(max_length=20, default="", null=True)
+    column_type = models.CharField(
+        max_length=10, choices=TYPE_CHOICES, default="full_name"
+    )
     schema = models.ForeignKey(SchemaBasicInfo, on_delete=models.CASCADE, null=True)
     int_from = models.PositiveIntegerField(default=0, null=True)
     int_to = models.PositiveIntegerField(default=0, null=True)
@@ -60,16 +66,15 @@ class SchemaColumns(BaseModel):
 
 
 class DataSets(BaseModel):
-    STATUS_CHOICES = (
-        ("ready", "ready"),
-        ("process", "processing")
-    )
+    STATUS_CHOICES = (("ready", "ready"), ("process", "processing"))
     schema = models.ForeignKey(SchemaBasicInfo, on_delete=models.CASCADE, null=True)
-    status = models.CharField(max_length=11, choices=STATUS_CHOICES, default="In process")
-    csv_file = models.CharField(max_length=300, default='', null=True)
+    status = models.CharField(
+        max_length=11, choices=STATUS_CHOICES, default="In process"
+    )
+    csv_file = models.CharField(max_length=300, default="", null=True)
     rows_number = models.PositiveIntegerField(default=0, null=True)
-    task_id = models.CharField(max_length=300, default='', null=True)
-    task_status = models.CharField(max_length=40, default='', null=True)
+    task_id = models.CharField(max_length=300, default="", null=True)
+    task_status = models.CharField(max_length=40, default="", null=True)
 
     def __str__(self):
         return str(self.schema)
