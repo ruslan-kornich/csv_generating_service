de69ec1e510252ad9ea32e338ce2ae3b17fec0b1
ruslan-kornich <korny4rus143@gmail.com>
2023-03-04 09:46:57 +0200

    urls

--- app/urls.py
+++ app/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('new-schema', views.new_schema, name="new_schema"),
     path('edit-schema/<int:id>/', views.edit_schema, name="edit_schema"),
     path('delete-schema/<int:id>/', views.delete_schema, name="delete_schema"),
-    path('add-schema', views.add_new_schema, name="add_new_schema"),
-    path('update-schema/<int:id>', views.update_schema, name="update_schema"),
+    path('add-schema', views.add_new_schema, name="adding_new_schema"),
+    path('update-schema/<int:id>', views.update_schema, name="updating_schema"),
     path('data-sets/<int:id>', views.data_sets, name="data_sets"),
-    ]
-
+    path('generate/<int:id>/', views.generate_data, name="generate_data")
+]
